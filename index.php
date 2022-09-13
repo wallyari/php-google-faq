ccc6838461cc267ca9bc9d0931b1b3c134483920
ariannadepasquale <depasqualearianna@gmail.com>
2022-09-13 15:38:14 +0200

    Add Ciclo foreach

--- index.php
+++ index.php
@@ -34,6 +34,13 @@ $faq=[
     "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
     ],
 ];
+// $array($faq) |as| $chiave($key) => $valore($listaFaq) - dati dell'elemento corrente
+// $array($listFaq) |as| $chiave($ans) => $valore($ans) - dati dell'elemento corrente
 
-
+foreach($faq as $key => $listFaq) {
+    echo "<h4>$key</h4>";
+    foreach($listFaq as $ans){
+        echo "<p>" . $ans .'<br>'."<p>"; 
+    }
+}
 
